7dd06b5680def9bc7f4814ea6ff64c4f9d062b29
Adrian Peral <adrian.peral@gmail.com>
2023-05-23 00:09:04 +0100

    dynamic data pass

--- Backend fCC Course/DjangoTest/TestOne/myapp/views.py
+++ Backend fCC Course/DjangoTest/TestOne/myapp/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
 # Create your views here.
+
+
+def index(request):
+    context = {
+        'name': 'Adrian',
+        'age': 33,
+        'nationality': 'ARBRITAESP'
+    }
+    return render(request, 'index.html', context)
